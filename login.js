6b40b496d63fa34d9ad6d1b888d3ac3581007276
rolpanca <57642594+rolpanca@users.noreply.github.com>
2026-08-11 17:12:55 -0300

    att pagina login

--- login.js
+++ login.js
@@ -5,5 +5,21 @@ const senha = document.getElementById('senha');
 formulario.addEventListener('submit', function(event) {
     event.preventDefault();
     const valorEmail = email.value;
+    const valorSenha = senha.value;
+
+    console.log(valorEmail);
+    console.log(valorSenha);
+
+    if(valorEmail === '' || valorSenha === '') {
+        alert('Preencha todos os campos.');
+        return;
+    }
+
+    if (!email.checkValidity()) {
+        alert('Digite um e-mail válido.');
+        return;
+    }
+
+    alert('Dados preenchidos corretamente!');
 });
 
